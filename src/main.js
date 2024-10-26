ef49e24229ee270cb6aea17191191e147af7b100
lin-dongyizhi7 <2985956026@qq.com>
2024-10-27 02:40:21 +0800

    调整

--- src/main.js
+++ src/main.js
@@ -43,18 +43,17 @@ controls.dampingFactor = 0.05;
 // 设置旋转速度
 // controls.autoRotate = 0.05;
 
-const addRing = (star, ringImg) => {
+const addRing = (ringImg) => {
     const textLoader = new THREE.TextureLoader();
-    const ringGeometry = new THREE.TorusGeometry(1.3, 0.1, 16, 100);
-    const ringMaterial = new THREE.MeshBasicMaterial({
-    map: textLoader.load(ringImg),
-    transparent: true,
-    opacity: 0.6, // 调整透明度
+    const ringGeometry = new THREE.TorusGeometry(13, 3, 3, 100);
+    const ringMaterial = new THREE.MeshStandardMaterial({
+        map: textLoader.load(ringImg),
+        wireframe: true,
     });
     const ring = new THREE.Mesh(ringGeometry, ringMaterial);
     ring.rotation.x = Math.PI / 2; // 旋转光环以水平放置
-    star.add(ring); // 将光环添加到土星组中
-}
+    return ring;
+};
 
 // 增加球体
 const addStar = (data) => {
@@ -62,6 +61,9 @@ const addStar = (data) => {
     const geometry = new THREE.SphereGeometry(data.size);
     const material = new THREE.MeshStandardMaterial({map: textLoader.load(data.img)});
     const star = new THREE.Mesh(geometry, material);
+    if (data.img === 'img/saturn_bg.jpg') {
+        // star.add(addRing(star, 'img/saturn_bg.jpg'));
+    }
     star.position.set(data.distance, 0, 0);
     star.rotateX(90);
     return star;
@@ -71,63 +73,63 @@ const star_data = [
         img: 'img/sun_bg.jpg',
         distance: 0,
         size: 20,
-        rotVelocity: 100,
+        rotVelocity: 4,
     },
     {
         img: 'img/mercury_bg.jpg',
         distance: 30,
         size: 1,
-        rotVelocity: 10,
-        velocity: 500
+        rotVelocity: 2,
+        velocity: 416
     },
     {
         img: 'img/venus_bg.jpg',
         distance: 34,
         size: 2,
-        rotVelocity: 1,
-        velocity: 100
+        rotVelocity: 0.4,
+        velocity: 163
     },
     {
         img: 'img/earth_bg.jpg',
         distance: 40,
         size: 3,
-        rotVelocity: 10,
-        velocity: 80
+        rotVelocity: 100,
+        velocity: 100
     },
     {
         img: 'img/mars_bg.jpg',
         distance: 48,
         size: 2,
-        rotVelocity: 9,
-        velocity: 60
+        rotVelocity: 97,
+        velocity: 54
     },
     {
         img: 'img/jupiter_bg.jpg',
         distance: 80,
         size: 12,
-        rotVelocity: 60,
-        velocity: 30
+        rotVelocity: 243,
+        velocity: 9
     },
     {
         img: 'img/saturn_bg.jpg',
         distance: 110,
         size: 9,
-        rotVelocity: 50,
-        velocity: 10
+        rotVelocity: 227,
+        velocity: 3.5
     },
     {
         img: 'img/uranus_bg.jpg',
         distance: 150,
         size: 8,
-        rotVelocity: 40,
-        velocity: 2.5
+        rotVelocity: 139,
+        velocity: 1.2
     },
     {
         img: 'img/neptune_bg.jpg',
         distance: 200,
         size: 7,
-        rotVelocity: 30,
-        velocity: 0.5
+        rotVelocity: 150,
+        velocity: 0.6
     }
 ]
 
@@ -192,8 +194,10 @@ function animate() {
     // 旋转
     // sun.rotation.y += stars[0].rotVelocity * 0.0002;
     orbits.forEach((orbit, index) => {
-        // orbit.rotation.z += star_data[index].velocity * 0.0001;
-        stars[index].rotation.y += star_data[index].rotVelocity * 0.0002;
+        if(index) {
+            orbit.rotation.z += star_data[index].velocity * 0.000001;
+        }
+        stars[index].rotation.y += star_data[index].rotVelocity * 0.000365;
     })
     // 渲染
     renderer.render(scene, camera);
